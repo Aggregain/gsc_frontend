59623ba8f9cddb758c543a5a07eb376c373c105a
mukhanovfox <mukhanovfox@gmail.com>
2025-05-05 02:17:52 +0500

    f

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import uiHelpers from './plugins/formatters'
+import formatters from './plugins/formatters'
 import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
@@ -8,4 +8,4 @@ import 'element-plus/dist/index.css'
 import "./assets/app.scss";
 import { Icon } from '@iconify/vue';
 
-createApp(App).use(uiHelpers).use(store).use(router).use(ElementPlus).component('Icon', Icon).mount('#app')
+createApp(App).use(formatters).use(store).use(router).use(ElementPlus).component('Icon', Icon).mount('#app')
